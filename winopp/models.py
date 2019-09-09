25b11ec72863df7c14854b2e0d9056c08c0e114a
rumahaah@outlook.com <rumahaah@outlook.com>
2019-09-09 23:10:02 +0700

    add ey

--- winopp/models.py
+++ winopp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Handover2pm(object):
+	"""docstring for Handover2pm"""
+	ho_id = models.AutoField(primary_key=True)
+	customer_name = models.CharField(max_length=250)
+	po_date = models.DateField()
--- winopp/views.py
+++ winopp/views.py
@@ -4,5 +4,5 @@ from django.http import HttpResponse
 # Create your views here.
 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+# def index(request):
+    # return HttpResponse("Hello, world. You're at the polls index.")
