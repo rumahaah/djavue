a8ff204af35fa3585d6b23e4801d10e85eb1ee56
rumahaah@outlook.com <rumahaah@outlook.com>
2019-10-22 06:55:27 +0700

    upuplg

--- winopp/admin.py
+++ winopp/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Handover2pm
+from .models import Handover2pm, Customer
 
 # Register your models here.
 
 admin.site.register(Handover2pm)
+admin.site.register(Customer)
--- winopp/models.py
+++ winopp/models.py
@@ -13,17 +13,34 @@ class Handover2pm(models.Model):
 			('5', 'Handover - Approval PM'),
 			('6', 'Handover - Approved'),
 		)
-	customer_name = models.CharField(max_length=250)
+	customer = models.ManyToManyField('Customer')
 	project_name = models.CharField(max_length=250)
 	po_date = models.DateField()
 	po_known_date = models.DateField()
-	initial_ho = models.DateField()
+	initial_ho = models.DateField(blank=True)
 	pmois_status = models.CharField(max_length=1, choices=status)
-	approve_ho = models.DateField()
-	remark = models.TextField()
+	approve_ho = models.DateField(blank=True)
+	remark = models.TextField(blank=True)
 
 	table_updated = models.DateField(auto_now=True, auto_now_add=False)
 	table_creation_timestamp = models.DateField(auto_now=False, auto_now_add=True)
 
 	def __str__(self):
 		return self.project_name
+
+class Customer(models.Model):
+	segment = (
+			('0', 'Banking 1'),
+			('1', 'Banking 2'),
+			('2', 'Finance Non-Bank'),
+			('3', 'Supply Chain'),
+			('4', 'Public Sector 1'),
+			('5', 'Public Sector 2'),
+			('6', 'Resources'),
+			('7', 'Telco'),
+		)
+	customer_name = models.CharField(max_length=100)
+	customersegment = models.CharField(max_length=1, choices=segment)
+
+	def __str__(self):
+		return self.customer_name
